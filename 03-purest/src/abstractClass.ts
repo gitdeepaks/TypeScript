3406abf93a4c423fca73ee3cb00946c55fb64367
gitdeepaks <deepaksankhyan92@outlook.com>
2023-04-06 20:49:39 +0530

    new

---
+++ 03-purest/src/abstractClass.ts
@@ -0,0 +1,27 @@
+abstract class TakePhoto {
+  constructor(public cameraMode: string, public filter: string) {}
+
+  abstract getSepia(): void;
+  getReelTime(): number {
+    //some complex calculation
+    return 8;
+  }
+}
+
+class Instagram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number
+  ) {
+    super(cameraMode, filter);
+  }
+
+  getSepia(): void {
+    console.log('Sepia');
+  }
+}
+
+const ds = new Instagram('test', 'Test', 4);
+
+ds.getReelTime();
