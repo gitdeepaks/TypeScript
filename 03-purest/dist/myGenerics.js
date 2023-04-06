6700d43c6b65954f6bec283bb393681093de8d07
gitdeepaks <deepaksankhyan92@outlook.com>
2023-04-06 21:04:16 +0530

    new

---
+++ 03-purest/dist/myGenerics.js
@@ -0,0 +1,20 @@
+"use strict";
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+identityThree(true);
+function identityFour(val) {
+    return val;
+}
+identityFour({
+    brand: 'Solimo',
+    type: 1,
+});
