5b211c34edfa915502f2d53fd61848c65a83c465
gitdeepaks <deepaksankhyan92@outlook.com>
2023-08-05 19:28:15 +0530

    new 🚀

---
+++ TypeSciprt_StratOver/Lesson-2/build/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+let myName = "Deepak";
+let meanngofLife;
+let isLoading;
+let album;
+myName = "Hohne";
+meanngofLife = 42;
+isLoading = true;
+album = 1991;
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ TypeSciprt_StratOver/Lesson-2/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Deepak";
+
+let meanngofLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Hohne";
+
+meanngofLife = 42;
+isLoading = true;
+
+album = 1991;
+
+album = true;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
