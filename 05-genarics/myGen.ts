08031d5237bd43d88fc2d53b3988a0756f3b794f
gitdeepaks <deepaksankhyan92@outlook.com>
2024-04-26 18:47:37 +0530

    new 🚀

--- 05-genarics/myGen.ts
+++ 05-genarics/myGen.ts
@@ -24,4 +24,51 @@ interface Bottle {
   type: number;
 }
 
-identityFour<Bottle>({});
+// identityFour<Bottle>({});
+
+
+function getSearchProducts<T>(params: T[]): T {
+  // do some Db opearations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+interface DataBase{
+    connection:string
+    username:string
+    password:string
+}
+
+const getMoreSearchProducts = <T,>(products:T[]):T=>{
+    // do some db opeartions
+    const myIndex = 4
+    return products[myIndex]
+}
+
+function anotherGenerics<T, U extends DataBase>(val1: T, val2: U): object {
+  return {
+    val1,
+    val2,
+  };
+}
+
+
+// anotherGenerics(3, {})
+
+interface Quiz {
+  name: string;
+  type: string;
+
+}
+
+interface Course {
+  name: string;
+  auther: string;
+  subject: string;
+
+}
+
+
+class Sellable<T> {
+  public cart: T[] = [];
+} 
