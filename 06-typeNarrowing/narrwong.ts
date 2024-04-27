ca7a6026ae4cc363425f9324aa006b2d16e98129
gitdeepaks <deepaksankhyan92@outlook.com>
2024-04-27 17:54:13 +0530

    new 🚀

--- 06-typeNarrowing/narrwong.ts
+++ 06-typeNarrowing/narrwong.ts
@@ -3,7 +3,7 @@ function detectTyes(val: number | string) {
     val.toLowerCase();
   }
 
-  return val + 3;
+  //   return val + 3;
 }
 
 function provideId(id: string | null) {
@@ -11,7 +11,7 @@ function provideId(id: string | null) {
     console.log("please provide the ID");
   }
 
-  id.toLowerCase();
+  //   id.toLowerCase();
 }
 
 interface User {
@@ -28,5 +28,22 @@ interface Admin {
 function isAdminAccount(account: User | Admin) {
   if ("isAdmin" in account) {
     return account.isAdmin;
+  }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet;
+    return "fish food";
+  } else {
+    pet;
+    return "bird food";
   }
 }
---
+++ 07-discreminated exhaustivechecking/index.ts
@@ -0,0 +1,38 @@
+interface Circle {
+  kind: "circle";
+  radius: number;
+}
+
+interface Square {
+  kind: "square";
+  side: number;
+}
+
+interface Rectangle {
+  kind: "rectangle";
+  width: number;
+  height: number;
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2;
+  }
+  return shape;
+}
+
+function getArea(shape: Shape) {
+  switch (shape.kind) {
+    case "circle":
+      return Math.PI * shape.radius ** 2;
+    case "square":
+      return shape.side ** 2;
+    case "rectangle":
+      return shape.width * shape.height;
+    default:
+      const _defaultforShape: never = shape;
+      return _defaultforShape;
+  }
+}
